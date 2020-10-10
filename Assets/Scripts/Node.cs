45d35eca7d735dc5954f4606b0e1411b4a14add4
Ramon Reyes <37812464+ramon-rr@users.noreply.github.com>
2020-10-10 10:16:09 -0700

    Added Node Design Classes
    Added User, Post, Node classes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using System.Device.Location;
+
+
+public class Node
+{
+    List<Post> posts;
+    GeoCoordinate location; //TODO: We might have classes that do this
+    public Node() 
+    {
+        posts = new ArrayList<Post>();
+        location = new GeoCoordinate();
+    }
+
+    public ArrayList<Post> GetPosts() 
+    {
+        return posts;
+    }
+
+    public Location GetLocation() 
+    {
+        return location;
+    }
+
+    public void addPost(Post post) 
+    {
+        posts.Add(post);
+    }
+
+
+   
+}
