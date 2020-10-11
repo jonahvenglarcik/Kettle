8068cec0366e27be4b7f8a292f74bf772beae816
Ramon Reyes <37812464+ramon-rr@users.noreply.github.com>
2020-10-11 09:16:31 -0700

    dev ready code
    commented out some classes. Uploaded libraries needed for Unity2019 and Mapbox integration

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using System.Device.Location;
+
 
 
 public class Node
 {
     List<Post> posts;
-    GeoCoordinate location; //TODO: We might have classes that do this
+  //  GeoCoordinate location; //TODO: We might have classes that do this
     public Node() 
     {
-        posts = new ArrayList<Post>();
-        location = new GeoCoordinate();
+        //posts = new ArrayList<Post>();
+       // location = new GeoCoordinate();
     }
 
-    public ArrayList<Post> GetPosts() 
+    public List<Post> GetPosts() 
     {
         return posts;
     }
 
-    public Location GetLocation() 
-    {
-        return location;
-    }
+    //public Location GetLocation() 
+    //{
+    //    return location;
+    //}
 
     public void addPost(Post post) 
     {
--- Assets/Scripts/User.cs
+++ Assets/Scripts/User.cs
@@ -2,42 +2,41 @@
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using System.Device.Location;
 using System.Threading;
 
 public class User
 {
     string realName; 
     string userName;
-    Radius currentRadius;
-    List<Chunk> currentChunks;
-    GeoCoordinate location;
-    List<Post> pinnedPosts;
-    Chat chatHistory;
+    //Radius currentRadius;
+    //List<Chunk> currentChunks;
+    //GeoCoordinate location;
+   // List<Post> pinnedPosts;
+   // Chat chatHistory;
 
-    public User(string realName, string userName, GeoCoordinate location) 
+    public User(string realName, string userName)//, GeoCoordinate location) 
     {
         this.realName = realName;
         this.userName = userName;
-        this.location = location;
-        currentRadius = null;
-        currentChunks = null;
-        pinnedPosts = null;
-        chatHistory = null;
+        //this.location = location;
+        //currentRadius = null;
+        //currentChunks = null;
+        //pinnedPosts = null;
+        //chatHistory = null;
     }
 
-    public User(string realName, string userName, GeoCoordinate location,
-        Radius currentRadius, ArrayList<Chunk> currentChunks,
-        ArrayList<Post> pinnedPosts, Chat chatHistory)
-    {
-        this.realName = realName;
-        this.userName = userName;
-        this.location = location;
-        this.currentRadius = currentRadius;
-        this.currentChunks = currentChunks;
-        this.pinnedPosts = pinnedPosts;
-        this.chatHistory = chatHistory;
-    }
+    //public User(string realName, string userName GeoCoordinate location,
+    //    Radius currentRadius, ArrayList<Chunk> currentChunks,
+    //    ArrayList<Post> pinnedPosts, Chat chatHistory)
+    //{
+    //    this.realName = realName;
+    //    this.userName = userName;
+    //    //this.location = location;
+    //    //this.currentRadius = currentRadius;
+    //    //this.currentChunks = currentChunks;
+    //    //this.pinnedPosts = pinnedPosts;
+    //    //this.chatHistory = chatHistory;
+    //}
 
     public string GetRealName() 
     {
@@ -49,14 +48,14 @@ public class User
         return userName;
     }
 
-    public Radius GetRadius() 
-    {
-        return currentRadius;
-    }
+    //public Radius GetRadius() 
+    //{
+    //    return currentRadius;
+    //}
 
-    public GeoCoordinate GetLocation()
-    {
-        return location;
-    }
+    //public GeoCoordinate GetLocation()
+    //{
+    //    return location;
+    //}
 
 }
