bcabdde007c4ba6652a197d53f504aafa638abe8
Jonah Venglarcik <jonahvenglarcik@gmail.com>
2020-10-12 23:30:00 -0700

    Started implementing button functionality

---
+++ Assets/Scripts/UI/NotesPanelButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NotesPanelButton : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+
+    public int SetButtonCount()
+    {
+        return 0;
+    }
+
+    public void DoButtonAction()
+    {
+        return;
+    }
+}
