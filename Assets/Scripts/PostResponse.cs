0260d82c9563a7b6af97751d129cd2be5c8d2f42
bradleysmithtx <bradleysmithtx@gmail.com>
2020-10-30 15:48:37 -0500

    added PostResonse class for JSON serializing

---
+++ Assets/Scripts/PostResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class PostResponse
+{
+    [JsonProperty("Author")] 
+    public UserResponse Author { get; set; }
+
+    [JsonProperty("Text")]
+    public string Text { get; set; }
+
+    [JsonProperty("TimeStamp")]
+    public DateTime TimeStamp { get; set; }
+
+    [JsonProperty("ApprovalRating")]
+    public int ApprovalRating { get; set; }
+
+    [JsonProperty("NodeLocation")]
+    public ResourceLocation NodeLocation { get; set; }
+}
