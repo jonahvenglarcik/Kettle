9662587c5ba75a6c7ac80ace4177078dec2512f9
Cephuez <46613677+Cephuez@users.noreply.github.com>
2020-10-31 17:11:11 -0700

    PinPost Update
    User can now write a title and a message. Post will be pinned on the map with its location and message

--- Assets/NewPinMethods.cs
+++ Assets/NewPinMethods.cs
@@ -1,36 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Mapbox.Examples;
+using Mapbox.Utils;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewPinMethods : MonoBehaviour
 {
+    public QuadTreeCameraMovement _QuadTreeCameraMovement;
     public GameObject newPinCanvas;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public InputField titleName;
+    public InputField message;
 
-    public void CreateNewPin()
+    private Vector3 pinPos;
+    private Vector2d mapLoc;
+    public void CreateNewPin(Vector3 pos, Vector2d mapLoc)
     {
-        //_PinCanvasActions.UpdatePin(newPin.GetComponent<PinPost>());
+        this.mapLoc = mapLoc;
+        pinPos = pos;
         newPinCanvas.SetActive(true);
+        
     }
 
     public void CloseNewPin()
     {
         newPinCanvas.SetActive(false);
+        //RemoveTextField();
     }
 
     public void PinPost()
     {
         // Instantiate the object back
+        String titleStr = titleName.text;
+        String messageStr = message.text;
         newPinCanvas.SetActive(false);
+
+        String[] pinInfo = new String[] {titleStr, messageStr};
+        
+        _QuadTreeCameraMovement.CreatePin(pinInfo,pinPos,mapLoc);
+        //RemoveTextField();
+    }
+
+    private void RemoveTextField()
+    {
+        titleName.text = "";
+        message.text = "";
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -24,34 +24,8 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void MovePerson()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector3(transform.position.x + 2.5f * Time.deltaTime, transform.position.y,
-                transform.position.z);
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position = new Vector3(transform.position.x + 2.5f* Time.deltaTime * -1, transform.position.y, transform.position.z);
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.Rotate(0,0,0);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.Rotate(0,0,0);
-        }
-    }
-
     private void SetMapChange()
     {
         _transformLocationProvider.ChangeLocation();
-        //var map = LocationProviderFactory.Instance.mapManager;
-        //map.ResetMap();
     }
 }
