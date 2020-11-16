bf40001a71932d9fca7d6cede73e3e99ba70c12f
Cephuez <46613677+Cephuez@users.noreply.github.com>
2020-11-16 05:26:32 -0700

    Forgot To Move To My Branch
    Updated the UI, but not done yes

--- Assets/NewPinMethods.cs
+++ Assets/NewPinMethods.cs
@@ -5,6 +5,8 @@ using Mapbox.Examples;
 using Mapbox.Utils;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class NewPinMethods : MonoBehaviour
 {
@@ -14,10 +16,30 @@ public class NewPinMethods : MonoBehaviour
     public InputField titleName;
     public InputField message;
 
+    public Text TitleText;
+    public Image PinIcon;
+    public Text PinMessage;
+    public Image[] DottedLines;
+    public Text TextSizeTracker;
+    public Image PinItImage;
+    
     private Vector3 pinPos;
     private Vector2d mapLoc;
+
+    public Color orangeColor;
+
+    private SpriteRenderer PinItImageColor;
+    void Update()
+    {
+        orangeColor = new Color(255,173,0);
+        CheckMessage();
+        CheckPinMessage();
+        PinItImage.color = orangeColor;
+    }
+    
     public void CreateNewPin(Vector3 pos, Vector2d mapLoc)
     {
+        //PinItImage.sprite
         this.mapLoc = mapLoc;
         pinPos = pos;
         newPinCanvas.SetActive(true);
@@ -47,5 +69,17 @@ public class NewPinMethods : MonoBehaviour
     {
         titleName.text = "";
         message.text = "";
+    }
+
+    public void CheckMessage()
+    {
+        
+    }
+
+    public void CheckPinMessage()
+    {
+        int pinMessageLength = PinMessage.text.Length;
+        TextSizeTracker.text = pinMessageLength + "/300";
+        Debug.Log(TextSizeTracker.text);
     }
 }
